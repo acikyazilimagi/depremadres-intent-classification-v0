c06903dc40234dd1ff898a78202f5957783f5c14
Halil Eralp Kocas <heralpkocas@gmail.com>
2023-02-08 15:17:38 +0100

    kural tabanli ve zero-shot tabanli modellerin beraber calistigi flowchart tamamen calisiyor ve db guncellendi.

--- main.py
+++ main.py
@@ -5,33 +5,33 @@ import run_zsc as zsc
 labels = ["KURTARMA", "YEMEK-SU", "GIYSI"]
 conn = pg_ops.connect_to_db()
 
+plot_data = {"key": labels, "count": [0 for i in range(len(labels))]}
+
 # is_done -> False/True based on processed or not
 # intent_result -> labels with separated by comma
 # Get data
 data = pg_ops.get_data(conn, 'tweets_depremaddress', ['id', 'full_text'], 'is_done = False')
-print("data: ", data)
 for row in data:
-    rule_based_labels = rbc.process_tweet(row)
+    rule_based_labels, plot_data = rbc.process_tweet(row, plot_data)
     intent_results = ""
-    print("=====================================")
     if rule_based_labels is not None:
         intent_results = ",".join(rule_based_labels)
-        print("intent results from rule-based: ", intent_results)
     elif intent_results == "":
         zsc_result = zsc.query(
                 {
                     "inputs": row[1],
                     "parameters": {"candidate_labels": labels},
                 })
-        print("zsc result: ", zsc_result)
-        label_scores = zsc_result["scores"]
-        intent_results = ",".join([zsc_result["labels"][i] for i in range(len(label_scores)) if label_scores[i] > 0.3])
-    print("intent results from zsc: ", intent_results)
-    print("=====================================")
-    # query = "UPDATE tweets_depremaddress SET intent=%s, is_done=True WHERE id=%s", (intent_results, row[0])
-    # print(query)
-    # cur = conn.cursor()
-    # cur.execute("UPDATE tweets_depremaddress SET intent_result=%s, is_done=True WHERE id=%s", (intent_results, row[0]))
-    # conn.commit()
+        if zsc_result is not None:
+            # sequence, labels, scores
+            if "scores" in zsc_result:
+                label_scores = zsc_result["scores"]
+                intent_results = ",".join([zsc_result["labels"][i] for i in range(len(label_scores)) if label_scores[i] > 0.3])
+    query = "UPDATE tweets_depremaddress SET intent=%s, is_done=True WHERE id=%s", (intent_results, row[0])
+    print(query)
+    cur = conn.cursor()
+    cur.execute("UPDATE tweets_depremaddress SET intent_result=%s, is_done=True WHERE id=%s", (intent_results, row[0]))
+    conn.commit()
 
+# rbc.draw_plot(plot_data)
 conn.close()
