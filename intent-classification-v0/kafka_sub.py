57531d90a61886cc7a894c9b8438afc4008f713d
yunusemre.emik <yunusemre.emik@logo.com.tr>
2023-02-09 17:15:11 +0300

    Add main functions ad error codes v2

--- intent-classification-v0/kafka_sub.py
+++ intent-classification-v0/kafka_sub.py
@@ -1,1 +1,30 @@
-﻿
+﻿from fastapi import FastAPI
+from aiokafka import AIOKafkaConsumer
+import asyncio
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+async def start_kafka_consumer(app):
+    consumer = AIOKafkaConsumer(
+        "your_topic",
+        bootstrap_servers="localhost:9092",
+        group_id="your_group_id",
+        auto_offset_reset="earliest"
+    )
+
+    # Start consuming
+    await consumer.start()
+
+    try:
+        # Poll for new messages
+        async for msg in consumer:
+            print(f"Consumed message: {msg.value}")
+    finally:
+        # Close the consumer
+        await consumer.stop()
+
+app.add_event_handler("startup", start_kafka_consumer)
